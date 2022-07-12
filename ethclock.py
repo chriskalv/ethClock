39c81f881215351dc34ba61737c90e93a107fe40
Chris <77923728+chriskalv@users.noreply.github.com>
2022-07-12 10:37:44 +0200

    Update ethclock.py

--- ethclock.py
+++ ethclock.py
@@ -1,41 +1,67 @@
-# Libraries for time and block height data creation
+##################################
+############ ETHCLOCK ############
+############## BY CK #############
+##################################
+
+# --------------------------------
+# --------Global Settings---------
+# Etherscan API data:
+etherscanAPI = "your etherscan.io API key"
+# OpenWeather API data:
+owmAPI = "your openweathermap.org API"
+latitude = "99.9999"
+longitude = "99.9999"
+# Locale
+tempscale = "metric"  # use either "metric" for °C or "imperial" for °F
+language = "english" # use either "english", "german", "spanish" or "french" for the display of weekdays in the corresponding language
+# --------------------------------
+
+# Libraries and global variables for...
+# ...clock functionality
 import time
+from threading import Thread
+current_time = "25:61"
+current_date = "00.00.0000"
+# ...API requests
 import requests
 import json
-from threading import Thread
-
-# Libraries for display
+current_weekdaytemp = "Keintag, 99°C"
+blockdisplay = "ETH Block 000000000"
+# ...display functionality
 import board
 import digitalio
 from PIL import Image, ImageDraw, ImageFont
 import adafruit_ssd1306
-
-# Set display parameters
+oled = adafruit_ssd1306.SSD1306_I2C(dispwidth, dispheight, board.I2C(), addr=0x3d, reset=digitalio.DigitalInOut(board.D4))
 dispwidth = 128
 dispheight = 64
 
-# I2C data
-oled = adafruit_ssd1306.SSD1306_I2C(dispwidth, dispheight, board.I2C(), addr=0x3d, reset=digitalio.DigitalInOut(board.D4))
-
 # Load fonts
-font = ImageFont.truetype('data-latin_modCK.ttf', 28)
-font1 = ImageFont.truetype('data-latin_modCK.ttf', 12)
-font2 = ImageFont.truetype('data-latin_modCK.ttf', 10)
+font = ImageFont.truetype('data-latin_mod.ttf', 28)
+font1 = ImageFont.truetype('data-latin_mod.ttf', 12)
+font2 = ImageFont.truetype('data-latin_mod.ttf', 10)
 
-# Set global variables
-current_time = "25:61"
-current_date = "00.00.0000"
-current_weekdaytemp = "Keintag, 23°C"
-blockdisplay = "ETH Block 000000000"
+# Establish locale for days of the week
+if language = "german":
+    weekdays = ("Sonntag","Montag","Dienstag","Mittwoch","Donnerstag","Freitag","Samstag")
+elif language = "spanish":
+    weekdays = ("Domingo","Lunes","Martes","Miércoles","Jueves","Viernes","Sábado")
+elif language = "french":
+    weekdays = ("Dimanche","Lundi","Mardi","Mercredi","Jeudi","Vendredi","Samedi")
+else:
+    weekdays = ("Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday")
+# Establish locale for temperature scale       
+if tempscale = "imperial":
+    tempunit = "°F"
+else:
+    tempunit = "°C"
 
-# Clear display.
+# Clear display and wait a second
 oled.fill(0)
 oled.show()
+time.sleep(1)
 
-# Give the environment some time
-time.sleep(4)
-
-# Function for creating a string that shows the current height of the ethereum blockchain and display it
+# Function for creating a string that shows the current height of the ethereum blockchain
 def blockheight():
     # Load global variables
     global blockdisplay
@@ -44,7 +70,6 @@ def blockheight():
     current_timeUNIX = int(time.time())
     
     # Generate Etherscan API URL and request the block height of the current UNIX timestamp
-    etherscanAPI = "8ZJABZXCVNFQJ8B3I68DZVBSQTJ6N38HVC"
     etherscanRequest = requests.get("https://api.etherscan.io/api?module=block&action=getblocknobytime&timestamp=" + str(current_timeUNIX) + "&closest=before&apikey=" + etherscanAPI).text
 
     # Load JSON from API request, search for block height and create displayable string
@@ -52,24 +77,21 @@ def blockheight():
     blockheight = response_info['result']
     blockdisplay = "ETH Block " + blockheight
 
-# Create and start individual loop to execute blockheight() every 60 seconds
+# Create and start an individual loop to execute blockheight() every 25 seconds
 def startblockloop():
     while True:
         blockheight()
-        time.sleep(60)        
+        time.sleep(25)        
 Thread(target=startblockloop).start()
 
-# Function for creating a string that shows the current temperature in defined area by usage of the OpenWeatherMap API
+# Function for creating a string that shows the current temperature in a defined area by usage of the OpenWeatherMap API
 def tempdate():
     # Load global variables
     global current_date
     global current_weekdaytemp
     
-    # Get weather data (temperature) by requesting OpenWeatherMap API for geographical data (Alter Lether Weg 6, Halen)
-    owmAPI = "2500d475f10d05588139d31f3e2f5f60"
-    latitude = "52.8572"
-    longitude = "8.1589"  
-    owmRequest = requests.get("https://api.openweathermap.org/data/2.5/weather?lat=" + latitude + "&lon=" + longitude + "&appid=" + owmAPI + "&units=metric")
+    # Get weather data (temperature) by requesting OpenWeatherMap API for geographical data (Alter Lether Weg 6, Halen)    
+    owmRequest = requests.get("https://api.openweathermap.org/data/2.5/weather?lat=" + latitude + "&lon=" + longitude + "&appid=" + owmAPI + "&units=" + tempscale)
       
     if owmRequest.status_code == 200:
         data = owmRequest.json()
@@ -77,16 +99,15 @@ def tempdate():
         temp = int(main['temp'])
     
     # Get/set current date and day of the week (by conversion)
-    current_weekdaynumber = int(time.strftime("%w"))
-    weekdays = ("Sonntag","Montag","Dienstag","Mittwoch","Donnerstag","Freitag","Samstag")
-    current_weekdaytemp = weekdays[current_weekdaynumber] + ", " + str(temp) + "°C"
+    current_weekdaynumber = int(time.strftime("%w"))        
+    current_weekdaytemp = weekdays[current_weekdaynumber] + ", " + str(temp) + tempunit
     current_date = time.strftime("%d.%m.%Y")
         
-# Create and start individual loop to execute temperature() every 171 seconds
+# Create and start an individual loop to execute tempdate() every minute
 def starttemploop():
     while True:
         tempdate()
-        time.sleep(171)        
+        time.sleep(60)        
 Thread(target=starttemploop).start()
 
 # Function for creating strings that show time
@@ -97,7 +118,7 @@ def timescript():
     # Get/set current 24h time
     current_time = time.strftime("%H:%M")
 
-# Create and start individual loop to execute timescript() every second
+# Create and start an individual loop to execute timescript() every second
 def starttimeloop():
     while True:
         timescript()
